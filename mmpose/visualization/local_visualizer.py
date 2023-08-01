2227ed859570349b50765b007eabdb1cc1285750
Tau-J <674106399@qq.com>
2023-08-01 11:22:47 +0800

    update dwpose

--- mmpose/visualization/local_visualizer.py
+++ mmpose/visualization/local_visualizer.py
@@ -687,6 +687,8 @@ class PoseLocalVisualizer(OpencvBackendVisualizer):
         if out_file is not None:
             mmcv.imwrite(drawn_img[..., ::-1], out_file)
         else:
+            print(name)
+            print(type(drawn_img))
             # save drawn_img to backends
             self.add_image(name, drawn_img, step)
 
