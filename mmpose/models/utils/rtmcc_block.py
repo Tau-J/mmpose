fa5575937465cc674059ab2406d6ea7aeb6cfa16
Tau-J <674106399@qq.com>
2023-08-24 21:16:37 +0800

    update

--- mmpose/models/utils/rtmcc_block.py
+++ mmpose/models/utils/rtmcc_block.py
@@ -162,7 +162,8 @@ class RTMCCBlock(nn.Module):
                  act_fn='SiLU',
                  bias=False,
                  use_rel_bias=True,
-                 pos_enc=False):
+                 pos_enc=False,
+                 use_dwc=False):
 
         super(RTMCCBlock, self).__init__()
         self.s = s
@@ -170,6 +171,17 @@ class RTMCCBlock(nn.Module):
         self.use_rel_bias = use_rel_bias
         self.attn_type = attn_type
         self.pos_enc = pos_enc
+        self.use_dwc = use_dwc
+
+        if use_dwc:
+            self.dwc = nn.Conv1d(
+                in_channels=self.e,
+                out_channels=self.e,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+                groups=self.e)
+
         self.drop_path = DropPath(drop_path) \
             if drop_path > 0. else nn.Identity()
 
@@ -285,7 +297,12 @@ class RTMCCBlock(nn.Module):
         if self.dropout_rate > 0.:
             kernel = self.dropout(kernel)
         # [B, K, K] x [B, K, e] -> [B, K, e]
-        x = u * torch.bmm(kernel, v)
+        x = torch.bmm(kernel, v)
+
+        if self.use_dwc:
+            x = x + self.dwc(v)
+
+        x = u * x
         # [B, K, e] -> [B, K, out_token_dims]
         x = self.o(x)
 
@@ -303,3 +320,237 @@ class RTMCCBlock(nn.Module):
             return self.res_scale(res_shortcut) + main_branch
         else:
             return self.drop_path(self._forward(x))
+
+
+class RTMCCBlock2(nn.Module):
+    """Gated Attention Unit (GAU) in RTMBlock.
+
+    Args:
+        num_token (int): The number of tokens.
+        in_token_dims (int): The input token dimension.
+        out_token_dims (int): The output token dimension.
+        expansion_factor (int, optional): The expansion factor of the
+            intermediate token dimension. Defaults to 2.
+        s (int, optional): The self-attention feature dimension.
+            Defaults to 128.
+        eps (float, optional): The minimum value in clamp. Defaults to 1e-5.
+        dropout_rate (float, optional): The dropout rate. Defaults to 0.0.
+        drop_path (float, optional): The drop path rate. Defaults to 0.0.
+        attn_type (str, optional): Type of attention which should be one of
+            the following options:
+
+            - 'self-attn': Self-attention.
+            - 'cross-attn': Cross-attention.
+
+            Defaults to 'self-attn'.
+        act_fn (str, optional): The activation function which should be one
+            of the following options:
+
+            - 'ReLU': ReLU activation.
+            - 'SiLU': SiLU activation.
+
+            Defaults to 'SiLU'.
+        bias (bool, optional): Whether to use bias in linear layers.
+            Defaults to False.
+        use_rel_bias (bool, optional): Whether to use relative bias.
+            Defaults to True.
+        pos_enc (bool, optional): Whether to use rotary position
+            embedding. Defaults to False.
+
+    Reference:
+        `Transformer Quality in Linear Time
+        <https://arxiv.org/abs/2202.10447>`_
+    """
+
+    def __init__(self,
+                 num_token,
+                 in_token_dims,
+                 out_token_dims,
+                 expansion_factor=2,
+                 s=128,
+                 eps=1e-5,
+                 dropout_rate=0.,
+                 drop_path=0.,
+                 attn_type='self-attn',
+                 act_fn='SiLU',
+                 bias=False,
+                 use_rel_bias=True,
+                 pos_enc=False):
+
+        super(RTMCCBlock2, self).__init__()
+        self.s = s
+        self.num_token = num_token
+        self.use_rel_bias = use_rel_bias
+        self.attn_type = attn_type
+        self.pos_enc = pos_enc
+        self.drop_path = DropPath(drop_path) \
+            if drop_path > 0. else nn.Identity()
+
+        self.e = int(in_token_dims * expansion_factor)
+        if use_rel_bias:
+            if attn_type == 'self-attn':
+                self.w = nn.Parameter(
+                    torch.rand([2 * num_token - 1], dtype=torch.float))
+            else:
+                self.a = nn.Parameter(torch.rand([1, s], dtype=torch.float))
+                self.b = nn.Parameter(torch.rand([1, s], dtype=torch.float))
+        self.o = nn.Linear(self.e, out_token_dims, bias=bias)
+
+        if attn_type == 'self-attn':
+            self.uv = nn.Linear(in_token_dims, 2 * self.e + self.s, bias=bias)
+            self.gamma = nn.Parameter(torch.rand((2, self.s)))
+            self.beta = nn.Parameter(torch.rand((2, self.s)))
+        else:
+            self.uv = nn.Linear(in_token_dims, self.e + self.s, bias=bias)
+            self.k_fc = nn.Linear(in_token_dims, self.s, bias=bias)
+            self.v_fc = nn.Linear(in_token_dims, self.e, bias=bias)
+            nn.init.xavier_uniform_(self.k_fc.weight)
+            nn.init.xavier_uniform_(self.v_fc.weight)
+
+        self.ln = ScaleNorm(in_token_dims, eps=eps)
+
+        nn.init.xavier_uniform_(self.uv.weight)
+
+        if act_fn == 'SiLU':
+            assert digit_version(TORCH_VERSION) >= digit_version('1.7.0'), \
+                'SiLU activation requires PyTorch version >= 1.7'
+
+            self.act_fn = nn.SiLU()
+        else:
+            self.act_fn = nn.ReLU()
+
+        if in_token_dims == out_token_dims:
+            self.shortcut = True
+            self.res_scale = Scale(in_token_dims)
+        else:
+            self.shortcut = False
+
+        self.sqrt_s = math.sqrt(s)
+
+        self.dropout_rate = dropout_rate
+
+        self.focusing_factor = 3
+        self.soft_plus = nn.Softplus()
+        self.scale = nn.Parameter(torch.zeros(size=(1, 1, self.s)))
+        self.dwc = nn.Conv1d(
+            in_channels=self.e,
+            out_channels=self.e,
+            kernel_size=5,
+            stride=1,
+            padding=2,
+            groups=self.e)
+
+        if dropout_rate > 0.:
+            self.dropout = nn.Dropout(dropout_rate)
+
+    def rel_pos_bias(self, seq_len, k_len=None):
+        """Add relative position bias."""
+
+        if self.attn_type == 'self-attn':
+            t = F.pad(self.w[:2 * seq_len - 1], [0, seq_len]).repeat(seq_len)
+            t = t[..., :-seq_len].reshape(-1, seq_len, 3 * seq_len - 2)
+            r = (2 * seq_len - 1) // 2
+            t = t[..., r:-r]
+        else:
+            a = rope(self.a.repeat(seq_len, 1), dim=0)
+            b = rope(self.b.repeat(k_len, 1), dim=0)
+            t = torch.bmm(a, b.permute(0, 2, 1))
+        return t
+
+    def _forward(self, inputs):
+        """GAU Forward function."""
+
+        if self.attn_type == 'self-attn':
+            x = inputs
+        else:
+            x, k, v = inputs
+
+        x = self.ln(x)
+
+        # [B, K, in_token_dims] -> [B, K, e + e + s]
+        uv = self.uv(x)
+        # uv = self.act_fn(uv)
+
+        if self.attn_type == 'self-attn':
+            # [B, K, e + e + s] -> [B, K, e], [B, K, e], [B, K, s]
+            u, v, base = torch.split(uv, [self.e, self.e, self.s], dim=2)
+            # [B, K, 1, s] * [1, 1, 2, s] + [2, s] -> [B, K, 2, s]
+            base = base.unsqueeze(2) * self.gamma[None, None, :] + self.beta
+
+            # if self.pos_enc:
+            #     base = rope(base, dim=1)
+            # [B, K, 2, s] -> [B, K, s], [B, K, s]
+            q, k = torch.unbind(base, dim=2)
+            if self.pos_enc:
+                k = rope(k, dim=1)
+
+        else:
+            # [B, K, e + s] -> [B, K, e], [B, K, s]
+            u, q = torch.split(uv, [self.e, self.s], dim=2)
+
+            k = self.k_fc(k)  # -> [B, K, s]
+            v = self.v_fc(v)  # -> [B, K, e]
+
+            if self.pos_enc:
+                q = rope(q, 1)
+                k = rope(k, 1)
+
+        q = self.act_fn(q) + 1e-6
+        k = self.act_fn(k) + 1e-6
+
+        scale = self.soft_plus(self.scale)
+        q = q / scale
+        k = k / scale
+
+        q_norm = q.norm(dim=-1, keepdim=True)
+        k_norm = k.norm(dim=-1, keepdim=True)
+        if float(self.focusing_factor) <= 6:
+            q = q**self.focusing_factor
+            k = k**self.focusing_factor
+        else:
+            q = (q / q.max(dim=-1, keepdim=True)[0])**self.focusing_factor
+            k = (k / k.max(dim=-1, keepdim=True)[0])**self.focusing_factor
+
+        q = (q / q.norm(dim=-1, keepdim=True)) * q_norm
+        k = (k / k.norm(dim=-1, keepdim=True)) * k_norm
+
+        # [B, K, s].permute() -> [B, s, K]
+        # [B, K, s] x [B, s, K] -> [B, K, K]
+
+        #    [B, K, s].sum(dim=1) -> [B, s]
+        # z: [B, K, s] x [B, s] -> [B, K]
+        z = 1 / (torch.einsum('b i c, b c -> b i', q, k.sum(dim=1)) + 1e-6)
+        # qk:[B, K, s] x [B, s, K] -> [B, K, K]
+        qk = torch.bmm(q, k.permute(0, 2, 1))
+
+        # [B, K, K] x [B, K, e], [B, K] -> [B, K, e]
+        x = torch.einsum('b i j, b j d, b i -> b i d', qk, v, z)
+
+        # [B, K, e] -> [B, e, K]
+        feature_map = v.permute(0, 2, 1)
+        feature_map = self.dwc(feature_map).permute(0, 2, 1)
+        x = x + feature_map
+        x = x * u
+        # [B, K, e] -> [B, K, out_token_dims]
+        x = self.o(x)
+
+        return x
+
+    def forward(self, x):
+        """Forward function."""
+
+        if self.shortcut:
+            if self.attn_type == 'cross-attn':
+                res_shortcut = x[0]
+            else:
+                res_shortcut = x
+            main_branch = self.drop_path(self._forward(x))
+            return self.res_scale(res_shortcut) + main_branch
+        else:
+            return self.drop_path(self._forward(x))
+
+
+b = RTMCCBlock2(17, 256, 256)
+inputs = torch.rand(2, 17, 256)
+outputs = b(inputs)
+print(outputs.shape)
